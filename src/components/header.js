17965d2020453517d5d0ff0a9e7c51daae00d2f4
Kevin <kqueenan@smu.edu>
2019-07-22 15:49:22 -0500

    part 2 begins

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,54 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
+
+import headerStyles from './header.module.scss'
 
 const Header = () => {
     return (
-        <header>
+        <header className={headerStyles.header}>
             <title>Kevin Queenan</title>
-            <h1>Kevin Queenan</h1>
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Kevin Queenan
+                </Link>
+            </h1>
             <nav>
-                <ul>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link to="/">Home</Link>
+                        <Link
+                            className={headerStyles.navItem}
+                            activeClassName={headerStyles.activeNavItem}
+                            to="/"
+                        >
+                            Home
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/blog">Blog</Link>
+                        <Link
+                            className={headerStyles.navItem}
+                            activeClassName={headerStyles.activeNavItem}
+                            to="/blog"
+                        >
+                            Blog
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link
+                            className={headerStyles.navItem}
+                            activeClassName={headerStyles.activeNavItem}
+                            to="/about"
+                        >
+                            About
+                        </Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact</Link>
+                        <Link
+                            className={headerStyles.navItem}
+                            activeClassName={headerStyles.activeNavItem}
+                            to="/contact"
+                        >
+                            Contact
+                        </Link>
                     </li>
                 </ul>
             </nav>
